e854e56e24afbe06c320a367419090fb402dd38d
jrbox <77578157+jrbox@users.noreply.github.com>
2023-05-21 14:20:00 +0000

    [FIX] ng serve for host

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-architecture 2';
+  title = 'angular-architecture';
 }
